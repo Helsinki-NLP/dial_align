7775e23649159d7b2dcece7f3c5afc77ced5c111
Yves Scherrer <yves.scherrer@gmail.com>
2022-08-19 16:22:17 +0300

    reorganize phrasetables

--- phrasetables/evaluate.sh
+++ phrasetables/evaluate.sh
@@ -3,7 +3,7 @@
 echo "" > evaluation.txt
 for PROJECT in archimob ndc skn; do
     for ALIGNER in eflomal giza levenshtein; do
-        python3 ../phrt_eval.py "$ALIGNER/$PROJECT/*.phrasetable.gz" >> evaluation.txt
+        python3 phrt_eval.py "$ALIGNER/$PROJECT/*.phrasetable.gz" >> evaluation.txt
     done
     echo "---------" >> evaluation.txt
 done
